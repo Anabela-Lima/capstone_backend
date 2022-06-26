b399b7216cf3be7ad6346383c6f695332bebac8f
Haining Zhen <hainingzhen@outlook.com>
2022-06-26 21:25:23 +0100

    began implementing user logic

--- src/main/java/com/sgone/capstone/project/controller/UserController.java
+++ src/main/java/com/sgone/capstone/project/controller/UserController.java
@@ -1,7 +1,35 @@
 package com.sgone.capstone.project.controller;
 
+import com.sgone.capstone.dto.response.StandardResponseDto;
+import com.sgone.capstone.project.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/user")
 public class UserController {
+
+    private UserService userService;
+
+    public UserController() {}
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/trip/new")
+    public ResponseEntity<StandardResponseDto<?>> createTrip() {
+        return null;
+    }
+
+
+    @PatchMapping("/trip/add_friend")
+    public ResponseEntity<StandardResponseDto<?>> addFriendToTrip() {
+        return null;
+    }
 }
--- src/main/java/com/sgone/capstone/project/service/UserService.java
+++ src/main/java/com/sgone/capstone/project/service/UserService.java
@@ -1,7 +1,29 @@
 package com.sgone.capstone.project.service;
 
+import com.sgone.capstone.project.model.Trip;
+import com.sgone.capstone.project.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
+
+    private UserRepository userRepository;
+
+    public UserService() {}
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public Trip createTrip() {
+        return null;
+    }
+
+
+    public Trip addFriendToTrip() {
+        return null;
+    }
+
 }
